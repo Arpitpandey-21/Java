56844cab050ca3abb36e88de81128bdf9ee53783
Arpit Pandey <arpitpandey8317@gmail.com>
2026-04-02 09:27:56 +0530

    Create Area.java

---
+++ Chapter 1 basic/Area.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Area {
+
+    public static void main(String args[]){
+        
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.println("Enter the radius");
+        double r = sc.nextDouble();
+        double area = 3.14*r*r;
+        System.out.println("Area of Circle="+area);
+    }
+}
