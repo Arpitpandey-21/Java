0f053b690db56db209f9e46adc03cd12e4b6b112
Arpit Pandey <arpitpandey8317@gmail.com>
2026-04-03 10:14:05 +0530

    Update Cat.java

--- Chapter 4 methods/Cat.java
+++ Chapter 4 methods/Cat.java
@@ -1,1 +1,15 @@
-
+public class Cat {
+    
+    public static void main(String[] args) {
+        
+        String name = "boca";
+        
+        name = catuppercase(name);
+        
+        System.out.println(name);
+    }
+    
+    public static String catuppercase(String name) {
+        return name.toUpperCase();
+    }
+}
