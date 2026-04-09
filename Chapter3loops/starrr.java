f5c1bd8acd02b46c47d941bd03f72fd0e32b1be7
Arpit Pandey <arpitpandey8317@gmail.com>
2026-04-09 10:13:21 +0530

    Create starrr.java

---
+++ Chapter3loops/starrr.java
@@ -0,0 +1,21 @@
+public class starrr {
+    public static void main(String[] args) {
+
+        int i, j;
+
+        for (i = 1; i <= 4; i++) {
+
+            for (j = 1; j <= 7; j++) {
+
+                if (j >= 5-i && j <= 3+i) {
+                    System.out.print("*");
+
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println(); 
+        }
+    }
+}
