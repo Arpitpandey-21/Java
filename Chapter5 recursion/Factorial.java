3d3f8f5d47c786894b06fa4480b116ccb3622c4d
Arpit Pandey <arpitpandey8317@gmail.com>
2026-04-09 10:40:38 +0530

    Implement factorial calculation using recursion

---
+++ Chapter5 recursion/Factorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the number= ");
+        int num = sc.nextInt();
+        
+        System.out.println("Factorial of " + num + " is: " + factorial(num));
+        
+        sc.close();
+    }
+    
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);   // Recursive call
+        }
+    }
+}
