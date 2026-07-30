ab8e5b5d13b6d63cedf65c72ba045901bf954514
Arpit Pandey <arpitpandey8317@gmail.com>
2026-07-30 22:19:14 +0530

    Implement array sum calculation in Sum.javaarray

--- Chapter 6 Array/Sum.java
+++ Chapter 6 Array/Sum.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class Sum{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the array size:");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        int sum = 0;
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();   // pehle input lo
+            sum = sum + arr[i];      // fir sum karo
+        }
+
+        System.out.println("Sum of array = " + sum);
+    }
+    
+}
